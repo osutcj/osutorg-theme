df4c6636b54d9bb13741b17b0484b7c81b55fb30
Tudor900 <tudor900f1@gmail.com>
2024-02-14 23:37:43 +0200

    python setup for helpers

---
+++ setup.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.copy('./helpers/matchRegex.js',  './../../../current/core/frontend/helpers/matchRegex.js')
+shutil.copy('./helpers/removeNavigationType.js',  './../../../current/core/frontend/helpers/removeNavigationType.js')
+
